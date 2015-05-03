20a92b0a4ee12c58df702bf2a66ede1d32328ee3
danny1984 <csgdhali@163.com>
2015-05-03 17:39:08 +0800

    commit test

--- nndl/nnetwork.py
+++ nndl/nnetwork.py
@@ -16,10 +16,28 @@ import numpy as np
 
 
 class NNetwork():
+    
     def __init__(self, sizes):
         self.num_layers = len(sizes);
         self.sizes      = sizes;
         self.biases     = [np.random.randn(y,1) for y in sizes[1:]];
         self.weights    = [np.random.randn(y,x)
                            for x,y in zip(sizes[:-1], sizes[1:]) ];
+    
+    def feedforword(self, a):
+        for b,w in zip(self.biases, self.weights):
+            a = sigmoid_vec(np.dot(w, a) + b);
+        return a;
+    
         
+def sigmoid(z):
+    """The sigmoid function."""
+    return 1.0/(1.0+np.exp(-z))
+
+sigmoid_vec = np.vectorize(sigmoid)
+
+def sigmoid_prime(z):
+    """Derivative of the sigmoid function."""
+    return sigmoid(z)*(1-sigmoid(z))
+
+sigmoid_prime_vec = np.vectorize(sigmoid_prime)
