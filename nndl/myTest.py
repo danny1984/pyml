883005b3198d2c99fb4079d7f7d8523d37144712
陆绩 <dehong.gdh@alibaba-inc.com>
2016-06-06 19:46:49 +0800

    udpate

--- nndl/myTest.py
+++ nndl/myTest.py
@@ -19,16 +19,16 @@ import mnist_loader
 import mynetwork
 
 training_data, validate_data, test_data = mnist_loader.load_data_wrapper()
-# chapter 1: 使用quadratic cost function
-#net = mynetwork.NNetwork([784, 30, 10])
-#net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+# chapter 1: use quadratic cost function
+net = mynetwork.NNetwork([784, 30, 10])
+net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
 
-# chapter 3: 使用cross-entropy cost function
-import network2
-net = network2.Network([784, 30, 10], cost=network2.CrossEntropyCost)
-net.large_weight_initializer()
-net.SGD(training_data[:1000], 400, 10, 0.5, evaluation_data=test_data, \
-        monitor_evaluation_accuracy=True, monitor_training_cost=True)
+# chapter 3: use cross-entropy cost function
+# import network2
+# net = network2.Network([784, 30, 10], cost=network2.CrossEntropyCost)
+# net.large_weight_initializer()
+# net.SGD(training_data[:1000], 400, 10, 0.5, evaluation_data=test_data, \
+#         monitor_evaluation_accuracy=True, monitor_training_cost=True)
 
 
 
